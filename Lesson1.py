2a1c1805dc86e26ca91cf3cce8f10517119ba6b9
modcomlearning <modcomlearning@gmail.com>
2020-11-07 14:53:14 +0300

    First Commit

---
+++ Lesson1.py
@@ -0,0 +1,29 @@
+#  variables - they store values in memory
+# Data Types - is the type of data stored in variables
+# We look at 3 types: int, float, string, boolean
+# variable_name = value
+
+number_one = 400    # int  - whole numbers
+number_two = 300.3  # float  - has decimals
+number_three = 200  # int
+employee_name = 'Joseph Modcom'  # strings
+paid = False  # boolean
+
+print('=====PRINTING=====')
+print(number_one)
+print(number_two)
+print(number_three)
+print(employee_name)
+print(paid)
+
+# confirm types
+print(type(paid))
+
+
+
+
+
+
+
+
+
---
+++ Lesson1a.py
@@ -0,0 +1,38 @@
+
+# Write a Python Prog. to find Body Mass Index
+# we need weight and height, name, age, nationality
+print('======BMI Calculator=======')
+weight = float(input('What is Your Weight - Kgs:'))
+height = float(input('What is Your Height - meters:'))
+name = str(input('What is your name:'))
+age = int(input('What is your age:'))
+nationality = str(input('Are you kenyan? True/False'))
+
+print('Received Weight is', weight, 'Kgs')
+print('Received Height is', height, 'meters')
+print('Your Names was', name)
+print('Received Age is', age, 'Years')
+print('Are you Kenyan? ', nationality)
+
+# above combination of strings and variables  - concatenation
+# Formula  - Arithmetic Operators
+bmi = weight/(height**2)
+print('Your Final BMI is ', round(bmi, 2))
+print('======Thank You=====')
+
+# More operations   - Comparison   - if, elif, else
+if bmi <= 17.5:
+    print('Underweight')
+
+elif bmi <= 22.5:
+    print('Normal')
+
+elif bmi <= 29.5:
+    print('Overweight')
+
+else:
+    print('Obese')  # over 29...
+
+
+# i6yhube
+
